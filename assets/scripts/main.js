5de5021a446bf42d494489a3aa9401e1aeb6d200
Skywriter626 <capella1067@gmail.com>
2022-04-23 23:12:21 +0300

    add initial files

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.testimonial-section__slider--wrapper').slick({
+        prevArrow: false,
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./assets/images/testimonial/arrow-next.svg" alt=""></button>',
+    });
+});
