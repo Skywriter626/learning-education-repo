2fcf7fa80dfc0524f2eaef55a2bed9d36a1ba3e5
Skywriter626 <capella1067@gmail.com>
2022-04-24 16:16:52 +0300

    add initial files

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,27 @@
 $(function(){
-    $('.testimonial-section__slider--wrapper').slick({
+    $('.testimonial-section__slider').slick({
         prevArrow: false,
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./assets/images/testimonial/arrow-next.svg" alt=""></button>',
+    });
+});
+
+$(document).ready(function(){
+    $('.slick-btn').click(function(){
+        var btn = $('.slick-btn').prop({disabled: true});
+        $('.testimonial-section__assessment-content:nth-child(1)').slideToggle({
+            duration: 500,
+            complete: function() {
+                btn.prop({disabled: false});
+            }
+        }); 
+    });
+    $('.slick-btn').click(function(){
+        var btn = $('.slick-btn').prop({disabled: true});
+        $('.testimonial-section__assessment-content:nth-child(2)').slideToggle({
+            duration: 500,
+            complete: function() {
+                btn.prop({disabled: false});
+            }
+        });
     });
 });
