60cb82adbd2d1967bbf4498f3337ffa7575cddea
Skywriter626 <capella1067@gmail.com>
2022-05-31 02:31:42 +0300

    add initial files

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,36 @@
+'use strict'
+
+const showELearnVideoPlayer = () => {
+    const playerWrapper = document.querySelector('.about-elearn-section-player--wrapper')
+    const player = playerWrapper.querySelector('.about-elearn-section-player').contentWindow
+    playerWrapper.classList.add('visible')
+    player.postMessage('{"event": "command", "func": "playVideo", "args": ""}', "*");
+}
+
+const stopELearnVideoPlayer = () => {
+    const playerWrapper = document.querySelector('.about-elearn-section-player--wrapper')
+    const player = playerWrapper.querySelector('.about-elearn-section-player').contentWindow
+    player.postMessage('{"event": "command", "func": "pauseVideo", "args": ""}', "*");
+    if (playerWrapper.classList.contains('visible')) {
+        playerWrapper.classList.remove('visible')
+    }
+}
+
+const initELearnVideoPlayer = () => {
+    const aboutELearnPlayBtn = document.querySelector('.about-elearn-section__play--wrapper')
+    aboutELearnPlayBtn.addEventListener('click', showELearnVideoPlayer)
+    const aboutELearnPlayerWrapper = document.querySelector('.about-elearn-section-player--wrapper')
+    aboutELearnPlayerWrapper.addEventListener('mouseout', stopELearnVideoPlayer)
+}
+
+const init = () => {
+    initELearnVideoPlayer()
+}
+
+
+window.addEventListener('DOMContentLoaded', init)
+
+
 $(function(){
     $('.testimonial-section__slider').slick({
         prevArrow: false,
