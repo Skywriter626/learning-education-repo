578584cc82d4f77ca84902221fc36d85a1c1f8ff
Skywriter626 <capella1067@gmail.com>
2022-05-31 09:13:45 +0300

    add initial files

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const showELearnVideoPlayer = () => {
+const playELearnVideoPlayer = () => {
     const playerWrapper = document.querySelector('.about-elearn-section-player--wrapper')
     const player = playerWrapper.querySelector('.about-elearn-section-player').contentWindow
     playerWrapper.classList.add('visible')
     player.postMessage('{"event": "command", "func": "playVideo", "args": ""}', "*");
 }
 
-const stopELearnVideoPlayer = () => {
+const pauseELearnVideoPlayer = () => {
     const playerWrapper = document.querySelector('.about-elearn-section-player--wrapper')
     const player = playerWrapper.querySelector('.about-elearn-section-player').contentWindow
     player.postMessage('{"event": "command", "func": "pauseVideo", "args": ""}', "*");
@@ -18,18 +18,41 @@ const stopELearnVideoPlayer = () => {
 
 const initELearnVideoPlayer = () => {
     const aboutELearnPlayBtn = document.querySelector('.about-elearn-section__play--wrapper')
-    aboutELearnPlayBtn.addEventListener('click', showELearnVideoPlayer)
+    aboutELearnPlayBtn.addEventListener('click', playELearnVideoPlayer)
     const aboutELearnPlayerWrapper = document.querySelector('.about-elearn-section-player--wrapper')
-    aboutELearnPlayerWrapper.addEventListener('mouseout', stopELearnVideoPlayer)
+    aboutELearnPlayerWrapper.addEventListener('mouseout', pauseELearnVideoPlayer)
+}
+
+const initModalWindowViewer = () => {
+    let modalTriggers = document.querySelectorAll('.modal-trigger');
+    modalTriggers.forEach(modalTrigger => {
+
+        let modal = modalTrigger.parentNode.querySelector('.modal')
+
+        let span = modal.getElementsByClassName("close")[0]
+
+        modalTrigger.onclick = function() {
+            modal.style.display = "block";
+        }
+
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    })
 }
 
 const init = () => {
     initELearnVideoPlayer()
+    initModalWindowViewer()
 }
 
-
 window.addEventListener('DOMContentLoaded', init)
-
 
 $(function(){
     $('.testimonial-section__slider').slick({
